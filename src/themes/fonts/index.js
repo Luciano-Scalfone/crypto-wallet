345503306401f63ea5611e44318c8137f3e8cf03
Luciano-Scalfone <luciano.scalfone@gmail.com>
2023-03-06 11:15:29 -0300

    Includes colors and fonts files

---
+++ src/themes/fonts/index.js
@@ -0,0 +1,14 @@
+const FONTS = {
+    H1: '48px/56px Roboto',
+    H2: '40px/48px Roboto',
+    H3: '32px/40px Roboto',
+    H4: '24px/32px Roboto',
+    H5: '20px/24px Roboto',
+    BODY_TEXT: 'bold 16px/24px Roboto',
+    LABEL: 'bold 14px/16px Roboto',
+    SMALL_LABEL: 'bold 12px/14px Roboto',
+    PARAGRAPH_REGULAR: '14px/24px Roboto',
+    PARAGRAPH_BOLD: 'bold 14px/24px Roboto',
+};
+
+export default FONTS;
