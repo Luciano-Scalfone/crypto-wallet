9b7c98ad3f07bbca10160d0960c4718ef438aadd
Luciano-Scalfone <luciano.scalfone@gmail.com>
2023-03-10 19:14:26 -0300

    change Get method methods to custom hook useFetch;

--- src/service/fetchData.ts
+++ src/service/fetchData.ts
@@ -1,13 +1,24 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 const baseUrl = "http://localhost:8080";
 
-export const getLatestCryptos = async (): Promise<any> => {
-  const currencies = await axios
-    .get(baseUrl + "/latest")
-    .then((response) => response.data);
+export const useFetch = (endPoint: string) => {
+  const [data, setData] = useState([]);
 
-  return currencies;
+  useEffect(() => {
+    const fetchLatestCryptos = async () => {
+      const cryptos = await axios
+      .get(`${baseUrl}${endPoint}`)
+      .then((response) => response.data);
+
+      setData(cryptos);
+    };
+
+    fetchLatestCryptos();
+  }, [endPoint]);
+
+  return [data];
 };
 
 export const addNewSubscription = async (email: string): Promise<any> => {
